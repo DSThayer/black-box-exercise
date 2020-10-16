15174f4ac6a938ec27c7a5cb63d4020d84ec3d79
Grant Pearce (Swansea Bay UHB - Pharmacy) <Grant.Pearce@wales.nhs.uk>
2020-10-16 10:26:38 +0100

    update code

--- get_numbersGP.R
+++ get_numbersGP.R
@@ -1,5 +1,5 @@
 #Provide a prompt for user & assign numbers to get_numbers
 get_numbers <- function() {
   n <- readline (prompt= "Please enter a series of numbers")
-  return(as.vector(get_numbers))
+  return(as.vector(n))
 }
