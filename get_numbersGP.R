01a5aa6476a0c7e6da5f8c30b2a4503128957e32
Grant Pearce (Swansea Bay UHB - Pharmacy) <Grant.Pearce@wales.nhs.uk>
2020-10-16 10:17:28 +0100

    Initial commit of get_numbers function

---
+++ get_numbersGP.R
@@ -0,0 +1,5 @@
+#Provide a prompt for user & assign numbers to get_numbers
+get_numbers <- function() {
+  n <- readline (prompt= "Please enter a series of numbers")
+  return(as.vector(get_numbers))
+}
