320df09701df6a162d35ce22a0cd25bd6270bbe0
Andre <andreng1@gmail.com>
2020-10-16 10:24:21 +0100

    check numeric vector exercise

--- R/AN check_numeric.R
+++ R/AN check_numeric.R
@@ -2,11 +2,13 @@
 #Checking the values within the vector that whether it is numeric or not
 
 check_numeric <- function(vector){
-  if (!is.na(as.numeric(vector))) {
-    return(TRUE)
-  }else
-    {
-     print("This is not a number")
-    return(FALSE) 
+  for(i in 1:length(vector)){
+   if (is.numeric(vector[i])) {
+     return(TRUE)
+   }else
+     {
+       print("There are values that are not a number")
+     return(FALSE) 
+     }
   }
 }
